4bc277253f46b00345164e22fda5557e637fba18
amannitp <amannitp272@gmail.com>
2023-10-27 09:05:54 +0530

    use of module.exports

--- notes-app/app.js
+++ notes-app/app.js
@@ -7,4 +7,24 @@ const fs=require('fs')
 //fs.appendFileSync('message.txt',' data to append ','utf-8','flash-true')
 
 
-require('./utils.js')  // import other file using require()
+//require('./utils.js')  // import other file using require()
+
+// const {firstName,lastName}=require('./utils.js')
+// console.log(firstName)
+// console.log(lastName)
+
+// const name=require('./utils.js')
+
+// console.log(name.firstName)
+// console.log(name.lastName)
+
+// console.log(name.printName())
+
+// name.fun()
+// console.log(name.add(5,6))
+// //console.log(name)
+
+
+const getNotes=require('./utils.js')
+
+getNotes()
--- notes-app/utils.js
+++ notes-app/utils.js
@@ -1,1 +1,26 @@
-console.log(" utils.js ")
+// console.log(" utils.js ")
+
+// const firstName="Aman"
+// const lastName="Raj"
+
+// function printName(){
+//     return firstName+" "+lastName;
+// }
+
+// function fun(){
+//     console.log(" fun ")
+// }
+
+// const add=function(a,b){
+//     return a+b;
+// }
+
+// module.exports={firstName,lastName,printName,fun,add}
+
+
+
+function createNotes(){
+    console.log(" notes are created successfully")
+}
+
+module.exports =createNotes;
