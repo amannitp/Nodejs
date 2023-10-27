e8c864b1cb02d7c27324888df03b674d4c47742d
amannitp <amannitp272@gmail.com>
2023-10-27 08:01:25 +0530

    import other file using require()

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,10 @@
 const fs=require('fs')
-fs.writeFileSync('note.txt','this file is  created by node js ')
-fs.writeFileSync('note.txt','this file is again created by node js ')  // replace existed text
+//fs.writeFileSync('note.txt','this file is  created by node js ')
+//fs.writeFileSync('note.txt','this file is again created by node js ')  // replace existed text
 
-fs.appendFileSync('challenge1.txt','append file ')
-fs.appendFileSync('challenge1.txt',' again append file ','utf-8') // message is concat with previou one
-fs.appendFileSync('message.txt',' data to append ','utf-8','flash-true')
+//fs.appendFileSync('challenge1.txt','append file ')
+//fs.appendFileSync('challenge1.txt',' again append file ','utf-8') // message is concat with previou one
+//fs.appendFileSync('message.txt',' data to append ','utf-8','flash-true')
+
+
+require('./utils.js')  // import other file using require()
