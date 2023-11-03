3b58f5ed52cf4ac997c39d064e0040502f104450
amannitp <amannitp272@gmail.com>
2023-11-03 11:26:30 +0530

    callback concept

--- playground/4-callbackfn.js
+++ playground/4-callbackfn.js
@@ -1,32 +1,41 @@
-setTimeout(()=>{
-    console.log(" Callback function")
-},1000)
+// setTimeout(()=>{
+//     console.log(" Callback function")
+// },1000)
 
 
-const names =["Aman","Amit","Ajay","vivek"]
+// const names =["Aman","Amit","Ajay","vivek"]
 
-const nameStartWithA=names.filter((name)=>{
-    return name.startsWith('Am')
-})
+// const nameStartWithA=names.filter((name)=>{
+//     return name.startsWith('Am')
+// })
 
-console.log(nameStartWithA)
+// console.log(nameStartWithA)
 
 
-const geoCode=(address,callback)=>{
+// const geoCode=(address,callback)=>{
 
-    setTimeout(()=>{
-        const data={
-            longitude:0,
-            latitude:200
-        }
+//     setTimeout(()=>{
+//         const data={
+//             longitude:0,
+//             latitude:200
+//         }
 
-   callback(data)
+//    callback(data)
   
 
-    },2000)
+//     },2000)
+// }
+
+// geoCode("Patna",(data)=>{
+//     console.log(data)
+// })
+
+const add=(a,b,callback)=>{
+    setTimeout(()=>{
+        callback(a+b)
+    })
 }
 
-geoCode("Patna",(data)=>{
-    console.log(data)
-})
-
+ add(3,1,(sum)=>{
+     console.log(sum)
+ })
--- weather-app/app.js
+++ weather-app/app.js
@@ -13,7 +13,7 @@
 
 // 
 
-// const request=require('request')
+ const request=require('request')
 
 // const url='https://api.weatherstack.com/current?access_key=6f08a9c6c2a41c4484ddfbbc374592c9&query=india'
 
@@ -27,17 +27,47 @@
 //     }
 // })
 
-const url="https://api.mapbox.com/geocoding/v5/mapbox.places/paris.json?access_token=pk.eyJ1IjoiYW1hbnJhajI1MDYiLCJhIjoiY2xvZ3BiOWlrMTE4MDJqcXBzbjFzaWY3diJ9.QMsSWcQ8OjMeF3BrsqS8Xg&limit=1"
+// const url="https://api.mapbox.com/geocoding/v5/mapbox.places/paris.json?access_token=pk.eyJ1IjoiYW1hbnJhajI1MDYiLCJhIjoiY2xvZ3BiOWlrMTE4MDJqcXBzbjFzaWY3diJ9.QMsSWcQ8OjMeF3BrsqS8Xg&limit=1"
 
-const request =require('request')
+// const request =require('request')
 
-request({url:url},(error,response)=>{
-    if(error){
-        console.log(" Enable to connect")
-    }else if(response.body.error){
-         console.log(" unable to fiund location")
-    }else{
-        console.log(response.body);
-    }
+// request({url:url},(error,response)=>{
+//     if(error){
+//         console.log(" Enable to connect")
+//     }else if(response.body.error){
+//          console.log(" unable to fiund location")
+//     }else{
+//         console.log(response.body);
+//     }
    
+// })
+
+const geoCode=(address,callback)=>{
+    const url=`https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoiYW1hbnJhajI1MDYiLCJhIjoiY2xvZ3BiOWlrMTE4MDJqcXBzbjFzaWY3diJ9.QMsSWcQ8OjMeF3BrsqS8Xg&limit=1`
+
+    request({url:url,json:true},(error,response)=>{
+        //console.log(response.body.features)
+
+        if(error){
+            callback(" Unable to connet sevice",undefined)
+        }else if(response.body.features.length==0){
+            callback("Unable to find location",undefined)
+
+        }else{
+            callback(undefined,response.body.features[0].center)
+        }
+    })
+
+
+
+}
+
+geoCode("India",(error,data)=>{
+   if(error===undefined){
+    console.log("longitude is "+data[0]);
+    console.log("latitude is "+data[1]);
+   }else{
+    console.log(error)
+   }
+    
 })
